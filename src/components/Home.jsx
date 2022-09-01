171f2b5ecddd5216bc6286cfa01c4516d9405c46
Carlos <carlosseguessa@gmail.com>
2022-09-01 11:55:15 -0300

    Diseño de la grilla echo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Pokemones from "./Pokemones";
 
 const Home = () => {
   return (
     <>
-      <header className="w-full px-6 flex flex-col">
-        <div className="w-full flex items-center justify-between">
+      <header className="w-full flex flex-col">
+        <div className="w-full flex items-center justify-between px-4">
           <div className="flex items-center  mt-9">
             <img className="w-10 mr-4" src="/img/Pokeball.png" alt="Pokeball" />
             <h1 className="font-bold text-3xl">Pokédex</h1>
@@ -16,10 +17,11 @@ const Home = () => {
             </div>
           </button>
         </div>
-        <div>
+        <div className="px-[16px]">
             <input className="w-full text-center mt-8 px-4 py-2 h-10 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-gray-500" type="text" placeholder="🔎 Buscar"/>
         </div>
       </header>
+        <Pokemones />
     </>
   );
 };
