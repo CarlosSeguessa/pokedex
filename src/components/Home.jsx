e62b761fdde0a9e53d0a503e0b16dc459f61b986
Mathias <mathiastorres7@gmail.com>
2022-09-05 11:47:59 -0300

    Boton de filtro por nombre y numero

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Pokemones from "./Pokemones";
+import { useState, useEffect } from "react";
 const Home = (props) => {
+  const [newlist, setNewList] = useState(props.pokemones);
+  const [flag, setFlag] = useState(true);
+
+  const ordenar = () => {
+    let sortedList;
+    if (flag) {
+      sortedList = [...newlist].sort((a, b) =>
+        a.name > b.name ? 1 : a.name < b.name ? -1 : 0
+      );
+    } else {
+      sortedList = [...newlist].sort((a, b) =>
+        a.id > b.id ? 1 : a.id < b.id ? -1 : 0
+      );
+    }
+    setNewList(sortedList);
+    setFlag(!flag);
+  };
+  //if (){
+  //   let newPokemonList = [...newlist].sort((a, b) =>
+  //   a.id > b.id ? 1 : a.id < b.id ? -1 : 0,
+  //   );
+  //  setNewList(newPokemonList);
+  // }
+
+  //if (newPokemonList[0] === newlist[0])
+  //  newPokemonList = [...newlist].sort((b, a) =>
+  //    a.name < b.name ? 1 : a.name > b.name ? -1 : 0
+  //  );
   return (
-    
     <>
       <header className="w-full flex flex-col">
         <div className="w-full flex items-center justify-between px-4">
@@ -10,18 +38,26 @@ const Home = (props) => {
             <img className="w-10 mr-4" src="/img/Pokeball.png" alt="Pokeball" />
             <h1 className="font-bold text-3xl">Pokédex</h1>
           </div>
-          <button>
+          <button onClick={ordenar}>
             <div className="mt-8 flex items-center justify-center">
               <p>#</p>
-              <img className="w-4" src="/icons/Arrow.svg" alt="Filtered by id" />
+              <img
+                className="w-4"
+                src="/icons/Arrow.svg"
+                alt="Filtered by id"
+              />
             </div>
           </button>
         </div>
         <div className="px-[16px]">
-            <input className="w-full text-center mt-8 px-4 py-2 h-10 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-gray-500" type="text" placeholder="🔎 Buscar"/>
+          <input
+            className="w-full text-center mt-8 px-4 py-2 h-10 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-gray-500"
+            type="text"
+            placeholder="🔎 Buscar"
+          />
         </div>
       </header>
-        <Pokemones pokemones={props.pokemones} />
+      <Pokemones pokemones={newlist} />
     </>
   );
 };
