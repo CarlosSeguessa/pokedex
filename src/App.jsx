1234ef9ade46183010f27505bb0742d0a2949ebf
Carlos <carlosseguessa@gmail.com>
2022-09-29 09:46:17 -0300

    arreglado el bug de que no aparecen los pokemones al cargar la app

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,52 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import PokemonDetails from "./components/PokemonDetails";
 import NotFound from "./components/NotFound";
+import AddPokemon from "./components/AddPokemon";
 
 function App() {
 
  //traer pokemones desde mi api
   const [pokemones, setPokemones] = useState([]);
+  const [loading, setLoading] = useState(true);
   
   const getpokemones = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/api/pokemones");
+try {
+  const response = await fetch("http://localhost:3000/api/pokemones");
+  const data = await response.json();
+  setPokemones(data.data);
+  setLoading(false);
+} catch (error) {
+  console.error(error)
+}
+    // try {
+    //   const response = await fetch("http://localhost:3000/api/pokemones");
 
-      if (!response.ok) {
-        throw new Error("Error en la peticion");
-      }
-        const responsefetch = await response.json();
+    //   if (!response.ok) {
+    //     throw new Error("Error en la peticion");
+    //   }
+    //     const responsefetch = await response.json();
       
-        setPokemones(responsefetch.data);
-        console.log(pokemones)
+    //     setPokemones(responsefetch.data);
+    //     console.log(pokemones)
         
-      }catch (error) {
-      console.error(error)
-    }
+    //   }catch (error) {
+    //   console.error(error)
+    // }
   };
+
+  
+
+
     
 
 useEffect(() => {
     getpokemones();
 }, []);
 
-
-
+//mientras no carguen los pokemones, mostrar un loading
+if (loading) {
+  return <h1>Loading...</h1>;
+} else {
   return (
     <Routes>
       <Route path="*" element={<NotFound />}></Route>
@@ -44,8 +60,9 @@ useEffect(() => {
       >
         <Route path=":pokemonId" element={<PokemonDetails />} />
       </Route>
+      <Route path="/addpokemon" element={<AddPokemon />} />
     </Routes>
   );
 }
-
+}
 export default App;
